92bbf7a109b62e2bee65c1d962004bf89d4187bd
NaokiOsako <naoki-osako@kwansei.ac.jp>
2019-03-28 14:27:51 +0900

    [Create]First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+# coding: utf-8
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content], image_name: "default_user.jpg")
+
+    if params[:image]
+      @post.image_name = "#{@post.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/food_images/#{@post.image_name}", image.read)
+    end
+    
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
+  end
+end
